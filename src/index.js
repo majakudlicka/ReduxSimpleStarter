54c28d9e585cd2dc123c98f7f2803c2784ad0a89
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-04-19 12:20:00 +0200

    create a simple youtube app based on React

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import _ from 'lodash';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDxWhL-Tv7c61FTaaSj8F7diPng2i5h0ww';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {videos: [],
+                  selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300)
+    return (<div>
+      <SearchBar onSearchTermChange={videoSearch} />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+      videos={this.state.videos} />
+      </div>);
+  }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.container'));
